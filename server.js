56cea108f1b1c00660f8cb1f7db6129d540cefa8
Me-ann Urgel <meann1_2@yahoo.com>
2020-02-19 20:17:35 +0800

    remove git on client side

--- server.js
+++ server.js
@@ -39,4 +39,4 @@ if (process.env.NODE_ENV === "production") {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`listening at port ${PORT}`));
+app.listen(PORT, () => console.log(`Listening at port ${PORT}`));
