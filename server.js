cae3085a46af74f3abda4df20c19f5f66eabae1a
Me-ann Urgel <meann1_2@yahoo.com>
2020-02-19 18:20:08 +0800

    change port

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`listening at port ${PORT}`));
